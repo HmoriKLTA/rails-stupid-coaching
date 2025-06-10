9168804a2966f1777ea42b3e85a4503cfc792b52
Hayao Mori <hayaomori312@gmail.com>
2025-06-10 23:13:21 +0900

    finished css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,7 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
+
     if @question == 'I am going to work'
       @answer = 'Great!'
     elsif @question.ends_with?("?")
